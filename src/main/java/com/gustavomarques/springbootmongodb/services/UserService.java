18d25b247cedf6785f31423689ddd3292b26f01d
GustavoMarquesC <gustavomarquescunha@gmail.com>
2024-02-26 20:18:33 -0300

    ID user

--- src/main/java/com/gustavomarques/springbootmongodb/services/UserService.java
+++ src/main/java/com/gustavomarques/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gustavomarques.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavomarques.springbootmongodb.domain.User;
 import com.gustavomarques.springbootmongodb.repository.UserRepository;
+import com.gustavomarques.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
